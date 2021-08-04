b05cfe28b4c863353db2d59cc2e067b890e5ef06
aganje1 <47575400+aganje1@users.noreply.github.com>
2021-08-03 20:48:45 -0500

    Create PLINQ.cs

---
+++ PLINQ/PLINQ.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PLINQ
+{
+    class Program
+    {
+        public const long N = 1000000000;
+        static void Main(string[] args)
+        {
+            long[] list = new long[N];
+
+            Random rand = new Random();
+            int i;
+
+            for (i = 0; i < N - 1; i++)
+            {
+                list[i] = rand.Next(0, 1001);
+            }
+
+            var watch = new System.Diagnostics.Stopwatch();
+            watch.Start();
+
+            IEnumerable<long> query =
+                from element in list.AsParallel()
+                where element >= 4 && element <= 350
+                select element;
+
+            watch.Stop();
+
+            Console.WriteLine($"QuickSort Execution Time: {watch.ElapsedMilliseconds} ms.");
+            Console.WriteLine($"QuickSort Execution Time: {watch.ElapsedMilliseconds * 1000000} ns.");
+
+            Console.WriteLine("Would you like to print list? Type Y/N.");
+            string printVar = Console.ReadLine();
+            if (printVar == "Y" || printVar == "y")
+            {
+                foreach (int value in query)
+                {
+                    Console.WriteLine(value);
+                }
+            }
+        }
+    }
+}
